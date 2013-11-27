ba9538b291683fd283eaecb83558b88eb9ae507c
Kevin Mutyaba <kmutyaba@yammer-inc.com>
2013-11-26 16:13:39 -0800

    added resources.rb file

--- lib/yammer/resources.rb
+++ lib/yammer/resources.rb
@@ -12,33 +12,11 @@
 # See the Apache Version 2.0 License for specific language governing
 # permissions and limitations under the License.
 
-require 'yammer/version'
-require 'yammer/error'
-require 'yammer/configurable'
-require 'yammer/api'
-require 'yammer/http_adapter'
-require 'yammer/client'
-require 'yammer/api_handler'
-require 'yammer/api_response'
-require 'yammer/resources'
-
-module Yammer
-  class << self
-    include Configurable
-    include ApiHandler
-
-    def to_s
-      "<#{self.name}: #{self.options.inspect}>"
-    end
-
-  private
-    def method_missing(method_name, *args, &block)
-      return super unless api_handler.respond_to?(method_name)
-      api_handler.send(method_name, *args, &block)
-    end
-  end
-end
-
-Yam = Yammer
-
-Yammer.reset!
+require 'yammer/resources/identity_map'
+require 'yammer/resources/base'
+require 'yammer/resources/user'
+require 'yammer/resources/thread'
+require 'yammer/resources/message'
+require 'yammer/resources/message_body'
+require 'yammer/resources/group'
+require 'yammer/resources/group_membership'
